09a2e81e0448fb61e5c43434e23b24118fbda812
Chulki Lee <chulki.lee@gmail.com>
2018-08-03 02:01:20 -0700

    mix new with_with_jason

---
+++ apps/with_with_jason/lib/with_with_jason.ex
@@ -0,0 +1,18 @@
+defmodule WithWithJason do
+  @moduledoc """
+  Documentation for WithWithJason.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WithWithJason.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/with_with_jason/test/with_with_jason_test.exs
@@ -0,0 +1,8 @@
+defmodule WithWithJasonTest do
+  use ExUnit.Case
+  doctest WithWithJason
+
+  test "greets the world" do
+    assert WithWithJason.hello() == :world
+  end
+end
