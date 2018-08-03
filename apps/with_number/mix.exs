6628041eb87c93f8eade00d17e5d14cebf242bd2
Chulki Lee <chulki.lee@gmail.com>
2018-08-03 02:25:09 -0700

    remove elixir version constraints

--- apps/with_number/mix.exs
+++ apps/with_number/mix.exs
@@ -9,7 +9,6 @@ defmodule WithNumber.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
